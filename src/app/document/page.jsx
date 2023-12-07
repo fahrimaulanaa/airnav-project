caea35a239ae594468a867d0a8a244b446a58e08
Fahri Maulana Al Ghazali <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2023-12-07 14:10:04 +0700

    Commented out file input onChange event

--- src/app/document/page.jsx
+++ src/app/document/page.jsx
@@ -259,7 +259,7 @@ export default function Profile() {
       E-KTP
     </label>
     <input
-      onChange={getFile}
+      // onChange={getFile}
       type="file"
       id="eKtpInput"
       className="hidden"
